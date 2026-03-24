56d31e653617af51b9ec2bc85bbd2d24616b5ec8
neurothos42 <neurothos.dev@gmail.com>
2026-03-24 15:21:10 +0530

    Initial commit

---
+++ server/src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/server', (req, res) => {
+    res.status(200).json({ msg: "Server is Running" })
+})
+
+if (process.env.NODE_ENV === 'production') {
+
+    const buildPath = path.join(__dirname, '../../client/dist');
+
+    app.use(express.static(buildPath));
+
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(buildPath, 'index.html'));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log("The server is running at port ", PORT)
+})
+
